cb9434e8040df141190536c7732cfd3bf587e0a2
Wallace Stevan <wallaceste@hotmail.com>
2023-08-09 15:33:55 -0300

    Commit fim Exercicio Lista

--- src/main/java/list/OperacoesBasicas/ListaTarefas.java
+++ src/main/java/list/OperacoesBasicas/ListaTarefas.java
@@ -36,4 +36,24 @@ public class ListaTarefas {
     public void obterDescricoesTarefas(){
         System.out.println(tarefaList);
     }
+
+    public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas();
+
+        System.out.println("O número total de elementos na lista é: " + listaTarefas.obterNumeroTotalTarefas());
+
+        listaTarefas.adicionarTarefa("Tarefa 1");
+        listaTarefas.adicionarTarefa("Tarefa 2");
+        listaTarefas.adicionarTarefa("Tarefa 3");
+
+        System.out.println("O número total de elementos na lista é: " + listaTarefas.obterNumeroTotalTarefas());
+
+        listaTarefas.removerTarefa("Tarefa 2");
+
+        System.out.println("O número total de elementos na lista é: " + listaTarefas.obterNumeroTotalTarefas());
+
+        listaTarefas.obterDescricoesTarefas();
+
+
+    }
 }
--- src/main/java/list/OperacoesBasicas/Tarefa.java
+++ src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -13,4 +13,9 @@ public class Tarefa {
     public String getDescricao() {
         return descricao;
     }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
 }
