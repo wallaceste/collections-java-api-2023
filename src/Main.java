2d631973ebe91455fe875775e38b721ffcf3d577
Wallace Stevan <wallaceste@hotmail.com>
2023-08-09 11:02:24 -0300

    Primeiro commit pc EPT

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi Brow!");
+        System.out.println("Teste alteração main pelo pc trabalho.");
     }
+
 }
